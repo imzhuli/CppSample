86668c255bb3c538df699ef5459ceb962320a98a
zhuli <imzhuli@vip.qq.com>
2021-11-16 18:08:41 +0800

    hahahhaaaaaa

--- lib/include/zec_ext/Lua/LuaWrap.hpp
+++ lib/include/zec_ext/Lua/LuaWrap.hpp
@@ -6,7 +6,7 @@
 
 ZEC_NS
 {
-    
+
     class xLuaStateWrapper
     {
     public:
@@ -31,7 +31,7 @@ ZEC_NS
         ZEC_INLINE std::enable_if_t<std::is_same_v<tArg, const char *>, tArg> GetAt(int Index) const { return luaL_checkstring(_LuaStatePtr, Index); }
         template<typename tArg>
         ZEC_INLINE std::enable_if_t<std::is_same_v<tArg, std::string>, tArg> GetAt(int Index) const { return luaL_checkstring(_LuaStatePtr, Index); }
-    
+
         ZEC_INLINE auto GetTop() const { return lua_gettop(_LuaStatePtr); }
         ZEC_INLINE auto SetTop(int Index) const { lua_settop(_LuaStatePtr, Index); }
         ZEC_INLINE auto Pop(int Number) const { lua_pop(_LuaStatePtr, Number); }
@@ -41,14 +41,14 @@ ZEC_NS
         ZEC_INLINE void Push(const char * StrValue) const { lua_pushstring(_LuaStatePtr, StrValue); }
         ZEC_INLINE void Push(const std::string& StrValue) const { lua_pushstring(_LuaStatePtr, StrValue.c_str()); }
         ZEC_INLINE void Push(int (*Func)(lua_State*)) const { lua_pushcfunction(_LuaStatePtr, Func); }
-        
+
         template<typename tArg>
         ZEC_INLINE std::enable_if_t<std::is_floating_point_v<tArg>> Push(tArg Number) { lua_pushnumber(_LuaStatePtr, Number); }
         template<typename...Args>
         ZEC_INLINE void PushF(const char * FmtStr, Args&&...args) const { lua_pushfstring(_LuaStatePtr, FmtStr, std::forward<Args>(args)...); }
 
         template<typename tFirstArg, typename...tOtherArgs>
-        ZEC_INLINE std::enable_if_t<sizeof...(tOtherArgs), int> Push(tFirstArg&& FirstArg, tOtherArgs&&...args) const { 
+        ZEC_INLINE std::enable_if_t<sizeof...(tOtherArgs), int> Push(tFirstArg&& FirstArg, tOtherArgs&&...args) const {
             Push(std::forward<tFirstArg>(FirstArg));
             Push(std::forward<tOtherArgs>(args)...);
             return 1 + (int)sizeof...(args);
@@ -61,26 +61,26 @@ ZEC_NS
         }
 
         ZEC_INLINE lua_Integer PopInt() const {
-            auto Top = lua_gettop(_LuaStatePtr); 
+            auto Top = lua_gettop(_LuaStatePtr);
             assert (Top);
-            auto Ret = lua_tointeger(_LuaStatePtr, Top); 
-            lua_pop(_LuaStatePtr, 1); 
+            auto Ret = lua_tointeger(_LuaStatePtr, Top);
+            lua_pop(_LuaStatePtr, 1);
             return Ret;
         }
 
         ZEC_INLINE lua_Number PopNumber() const {
-            auto Top = lua_gettop(_LuaStatePtr); 
+            auto Top = lua_gettop(_LuaStatePtr);
             assert (Top);
             auto Ret = lua_tonumber(_LuaStatePtr, Top);
-            lua_pop(_LuaStatePtr, 1); 
+            lua_pop(_LuaStatePtr, 1);
             return Ret;
         }
 
         ZEC_INLINE std::string PopStr() const {
-            auto Top = lua_gettop(_LuaStatePtr); 
+            auto Top = lua_gettop(_LuaStatePtr);
             assert (Top);
-            auto Ret = lua_tostring(_LuaStatePtr, Top); 
-            lua_pop(_LuaStatePtr, 1); 
+            auto Ret = lua_tostring(_LuaStatePtr, Top);
+            lua_pop(_LuaStatePtr, 1);
             return Ret;
         }
 
@@ -104,22 +104,32 @@ ZEC_NS
         ZEC_INLINE bool Execute(const char * CodeStr) const { return LUA_OK == luaL_dostring(_LuaStatePtr, CodeStr); }
         ZEC_INLINE bool ExecuteFile(const char * Filename) const { return LUA_OK == luaL_dofile(_LuaStatePtr, Filename); }
         template<size_t ResultNumber = 0, typename...tArgs>
-        ZEC_INLINE void Call(const char * name, tArgs&&...args) {
-            lua_getglobal(_LuaStatePtr, name);        
+        ZEC_INLINE void CallN(const char * name, tArgs&&...args) const {
+            lua_getglobal(_LuaStatePtr, name);
             Push(std::forward<tArgs>(args)...);
-            lua_call(_LuaStatePtr, sizeof...(args), ResultNumber);        
+            lua_call(_LuaStatePtr, sizeof...(args), ResultNumber);
+        }
+
+        template<typename...tRetVars, typename...tArgs>
+        ZEC_INLINE auto Call(const char * name, tArgs&&...args) const {
+            CallN<sizeof...(tRetVars)>(name, std::forward<tArgs>(args)...);
+            return Pop<tRetVars...>();
         }
 
     protected:
         lua_State * _LuaStatePtr = nullptr;
+
+    public:
+        template<typename...tRetVars>
+        ZEC_INLINE auto Get() const { return xGet<tRetVars...>{this}; }
 
     private:
         template<size_t MemberIndex, typename tTuple>
         ZEC_INLINE void _FillTuple(tTuple & Tuple, int StackOffset) const {
             static_assert(!std::is_same_v<std::remove_reference_t<decltype(std::get<MemberIndex>(Tuple))>, const char *>);
             std::get<MemberIndex>(Tuple) = GetAt<std::remove_reference_t<decltype(std::get<MemberIndex>(Tuple))>>(StackOffset);
-            if constexpr(MemberIndex < std::tuple_size<tTuple>() - 1) {            
-                _FillTuple<MemberIndex + 1>(Tuple, StackOffset + 1);            
+            if constexpr(MemberIndex < std::tuple_size<tTuple>() - 1) {
+                _FillTuple<MemberIndex + 1>(Tuple, StackOffset + 1);
             }
         }
     };
--- sample/lua/sample_lua.cpp
+++ sample/lua/sample_lua.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int LuaCallbackSubSub(lua_State * LP)
 {
 	auto W = xLuaStateWrapper(LP);
+	auto [p1, p2, p3] = W.Pop<int, int, int>();
+	cout << "p1=" << p1 << ", p2=" << p2 << ", p3=" << p3 << endl;
 	return W.Push(1, 2, "Hello R3");
 }
 
@@ -15,8 +17,7 @@ int main(int, char *[])
 	xLuaState LuaState;
 
 	LuaState.SetGlobal("global_callback_subsub", LuaCallbackSubSub);
-	LuaState.Call<3>("global_callback_subsub");	
-	auto [R1,R2,R3] = LuaState.Pop<int, int, std::string>();
+	auto [R1,R2,R3] = LuaState.Call<int, int, std::string>("global_callback_subsub", 1, 2, 3);
 	cout << "Results: " << R1 << ", " << R2 << ", " << R3 << endl;
 
 	return 0;
