b36514853c6ddfd23f2220c9cdc46922175f9184
xelee.zhuli <imzhuli@vip.qq.com>
2022-02-14 19:05:19 +0800

    add socks5client init (connect)

--- lib/include/zec_ext/Net/Socks5Proxy.hpp
+++ lib/include/zec_ext/Net/Socks5Proxy.hpp
@@ -4,6 +4,7 @@
 #include <asio/asio.hpp>
 #include <atomic>
 #include <string_view>
+#include <string>
 
 ZEC_NS
 {
@@ -13,21 +14,35 @@ ZEC_NS
         virtual bool CheckUserPass(const std::string_view & Username, const std::string_view & Password) = 0;
     };
 
+    class xSocks5Client
+    : xNonCopyable
+    {
+    public:
+        bool Init(asio::io_context * IoContextPtr, const asio::ip::tcp::endpoint & TargetAddress, const std::string_view & Username, const std::string_view & Password);
+        void Clean();
+
+    protected:
+        virtual void ConnectionHandler(const asio::error_code& ErrorCode);
+
+    private:
+        asio::io_context *              _IoContextPtr {};
+        xHolder<asio::ip::tcp::socket>  _SocketHolder {};
+        std::string                     _Username {};
+        std::string                     _Password {};
+    };
+
     class xSocks5Proxy
     : xNonCopyable
     {
     public:
         bool Init(asio::io_context * IoContextPtr, const asio::ip::tcp::endpoint & BindAddress, iProxyAuth * AuthCallback);
         void Clean();
-        void Run();
-        void Stop();
 
     private:
-        std::atomic_bool                   _StopFlag = {};
-        iProxyAuth *                       _AuthCallback = {};
         asio::io_context *                 _IoContextPtr = {};
         asio::ip::address                  _Address = {};
         xHolder<asio::ip::tcp::acceptor>   _AcceptorHolder = {};
+        iProxyAuth *                       _AuthCallback = {};
     };
 
 }
--- lib/src/private_zec_ext/Net/Socks5Proxy.cpp
+++ lib/src/private_zec_ext/Net/Socks5Proxy.cpp
@@ -3,6 +3,41 @@
 ZEC_NS
 {
 
+    bool xSocks5Client::Init(asio::io_context * IoContextPtr, const asio::ip::tcp::endpoint & TargetAddress, const std::string_view & Username, const std::string_view & Password)
+    {
+        try {
+            _SocketHolder.New(*IoContextPtr);
+            _Username = Username;
+            _Password = Password;
+            _SocketHolder->non_blocking(true);
+            _SocketHolder->async_connect(TargetAddress, [this](const asio::error_code& ErrorCode){ConnectionHandler(ErrorCode);});
+        }
+        catch (...) {
+            if (_SocketHolder.IsValid()) {
+                _SocketHolder.Delete();
+            }
+            Reset(_IoContextPtr);
+            Reset(_Username);
+            Reset(_Password);
+            return false;
+        }
+        return true;
+    }
+
+    void xSocks5Client::Clean()
+    {
+        assert(_SocketHolder.IsValid());
+        _SocketHolder.Delete();
+        Reset(_IoContextPtr);
+        Reset(_Username);
+        Reset(_Password);
+    }
+
+    void xSocks5Client::ConnectionHandler(const asio::error_code& ErrorCode)
+    {
+        Todo();
+    }
+
     bool xSocks5Proxy::Init(asio::io_context * IoContextPtr, const asio::ip::tcp::endpoint & BindAddress, iProxyAuth * AuthCallback)
     {
         assert(AuthCallback);
@@ -16,9 +51,12 @@ ZEC_NS
         }
         catch (...)
         {
-            if (!_AcceptorHolder.IsValid()) {
+            if (_AcceptorHolder.IsValid()) {
                 _AcceptorHolder.Delete();
             }
+            Reset(_Address);
+            Reset(_IoContextPtr);
+            Reset(_AuthCallback);
             return false;
         }
         return true;
@@ -28,6 +66,9 @@ ZEC_NS
     {
         assert(_AcceptorHolder.IsValid());
         _AcceptorHolder.Delete();
+        Reset(_Address);
+        Reset(_IoContextPtr);
+        Reset(_AuthCallback);
     }
 
 }
