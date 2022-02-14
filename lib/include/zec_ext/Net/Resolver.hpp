19094deaca0b88df7b193e614d7e9387024f8bbf
xelee.zhuli <imzhuli@vip.qq.com>
2022-02-14 19:26:12 +0800

    add resolver

---
+++ lib/include/zec_ext/Net/Resolver.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <zec/Common.hpp>
+#include <asio/asio.hpp>
+#include <atomic>
+#include <string_view>
+#include <string>
+
+ZEC_NS
+{
+
+    class xResolver
+    : xNonCopyable
+    {
+
+    };
+
+}
---
+++ lib/src/private_zec_ext/Net/Resolver.cpp
@@ -0,0 +1,1 @@
+#include <zec_ext/Net/Resolver.hpp>
