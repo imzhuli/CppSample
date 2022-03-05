d9318c6deebbef40241a05f59a60d448b3337938
xelee.zhuli <imzhuli@vip.qq.com>
2022-03-05 17:53:48 +0800

    netbase

---
+++ lib/include/zec_ext/IO/NetBase.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <zec/Common.hpp>
+
+ZEC_NS
+{
+
+    struct xNetAddress {
+        enum : uint8_t {
+            eUnknown, eIpv4, eIpv6
+        } Type = eUnknown;
+
+        union {
+            ubyte Ipv4[4];
+            ubyte Ipv6[16];
+        };
+
+        ZEC_INLINE bool IsV4() const { return Type == eIpv4; }
+        ZEC_INLINE bool IsV6() const { return Type == eIpv6; }
+        ZEC_INLINE operator bool () const { return Type != eUnknown; }
+        ZEC_API_MEMBER std::string ToString() const;
+
+        ZEC_API_STATIC_MEMBER xNetAddress Make(const char * IpStr);
+        ZEC_API_STATIC_MEMBER xNetAddress MakeV4(const char * IpStr);
+        ZEC_API_STATIC_MEMBER xNetAddress MakeV6(const char * IpStr);
+    };
+
+}
---
+++ lib/src/private_zec_ext/IO/NetBase.cpp
@@ -0,0 +1,4 @@
+#include <zec_ext/IO/NetBase.hpp>
+
+ZEC_NS
+{}
