54bd325c64d402982137f0fea9eeca21dc39fc94
xelee.zhuli <imzhuli@vip.qq.com>
2022-03-23 15:21:41 +0800

    not throw on GetAddress

--- lib/src/private_zec_ext/IO/TcpConnection.hpp
+++ lib/src/private_zec_ext/IO/TcpConnection.hpp
@@ -31,8 +31,8 @@ ZEC_NS
         ZEC_API_MEMBER xTcpSocketContext(xIoHandle Handle);
         ZEC_API_MEMBER ~xTcpSocketContext();
 
-        ZEC_INLINE tcp::endpoint GetLocalAddress() const { return _Socket.local_endpoint(); }
-        ZEC_INLINE tcp::endpoint GetRemoteAddress() const { return _Socket.remote_endpoint(); }
+        ZEC_INLINE tcp::endpoint GetLocalAddress() const { return _Socket.local_endpoint(X2Ref(xAsioError{})); }
+        ZEC_INLINE tcp::endpoint GetRemoteAddress() const { return _Socket.remote_endpoint(X2Ref(xAsioError{})); }
 
         ZEC_API_MEMBER void ResizeSendBuffer(size_t Size) { asio::socket_base::send_buffer_size Option(Size); _Socket.set_option(Option); }
         ZEC_API_MEMBER void ResizeReceiveBuffer(size_t Size) { asio::socket_base::receive_buffer_size Option(Size); _Socket.set_option(Option); }
