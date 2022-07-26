b80207b160e9d1e23ebf129497637d358bfd3043
xelee <imzhuli@vip.qq.com>
2022-07-27 05:35:26 +0800

    xNone private base

--- lib/include/zec/Common.hpp
+++ lib/include/zec/Common.hpp
@@ -104,7 +104,7 @@ ZEC_NS
 		struct xAbstract { protected: constexpr xAbstract() = default; virtual ~xAbstract() = default; xAbstract(xAbstract &&) = delete; };
 		struct xNonCopyable { protected: constexpr xNonCopyable() = default; ~xNonCopyable() = default; xNonCopyable(xNonCopyable &&) = delete; };
     	struct xNonCatchable final { private: constexpr xNonCatchable() = default; ~xNonCatchable() = default; };
-		struct xNone final : xNonCopyable {};
+		struct xNone final : private xNonCopyable {};
 
 		constexpr struct xNone None;
 		constexpr struct xNoInit final {} NoInit {};
