77a5d05510430b5080c8ec15fa456fe2f872dada
xe.lee <imzhuli@gmail.com>
2022-12-01 22:35:55 +0800

    sync linux

--- lib/src/private_xel_ext/IO/TcpConnection_Linux.cpp
+++ lib/src/private_xel_ext/IO/TcpConnection_Linux.cpp
@@ -36,6 +36,48 @@ X_NS
 
     bool xTcpConnection::Init(xIoContext * IoContextPtr, const xNetAddress & Address, iListener * ListenerPtr)
     {
+        int AF = AF_UNSPEC;
+        sockaddr_storage AddrStorage;
+        size_t AddrLen = 0;
+        memset(&AddrStorage, 0, sizeof(AddrStorage));
+        if (Address.IsV4()) {
+            auto & Addr4 = (sockaddr_in&)AddrStorage;
+            Addr4.sin_family = AF_INET;
+            Addr4.sin_addr = (decltype(sockaddr_in::sin_addr)&)(Address.Ipv4);
+            Addr4.sin_port = htons(Address.Port);
+            AddrLen = sizeof(sockaddr_in);
+            AF = AF_INET;
+        } else if (Address.IsV6()) {
+            auto & Addr6 = (sockaddr_in6&)AddrStorage;
+            Addr6.sin6_family = AF_INET6;
+            Addr6.sin6_addr = (decltype(sockaddr_in6::sin6_addr)&)(Address.Ipv6);
+            Addr6.sin6_port = htons(Address.Port);
+            AddrLen = sizeof(sockaddr_in6);
+            AF = AF_INET6;
+        }
+        else {
+            return false;
+        }
+
+        assert(_Socket == InvalidSocket);
+        _Socket = InvalidSocket;
+
+        auto FailSafe = xScopeGuard{[this]{
+            if (_Socket != InvalidSocket) {
+                XelCloseSocket(X_DEBUG_STEAL(_Socket, InvalidSocket));
+            }
+            X_DEBUG_RESET(_IoContextPtr);
+            X_DEBUG_RESET(_ListenerPtr);
+        }};
+
+        _Socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (-1 == Socket) {
+            X_DEBUG_PRINTF("Failed to create socket\n");
+            return false;
+        }
+
+        int flags = fcntl(NativeHandle, F_GETFL);
+        fcntl(NativeHandle, F_SETFL, flags | O_NONBLOCK);
         return false;
     }
 
