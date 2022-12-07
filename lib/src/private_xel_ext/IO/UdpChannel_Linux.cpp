38564054085da4e344e68ee56ee498ba656709fb
xelee <imzhuli@vip.qq.com>
2022-12-07 18:01:51 +0800

    udp post data

--- lib/src/private_xel_ext/IO/UdpChannel_Linux.cpp
+++ lib/src/private_xel_ext/IO/UdpChannel_Linux.cpp
@@ -20,8 +20,26 @@ X_NS
 		X_DEBUG_RESET(_ListenerPtr);
 	}
 
+	void xUdpChannel::PostData(const void * DataPtr, size_t DataSize, const xNetAddress & Address)
+	{
+        sockaddr_storage AddrStorage = {};
+        size_t AddrLen = 0;
+        memset(&AddrStorage, 0, sizeof(AddrStorage));
+        if (Address.IsV4()) {
+            auto & Addr4 = (sockaddr_in&)AddrStorage;
+            Addr4.sin_family = AF_INET;
+            Addr4.sin_addr = (decltype(sockaddr_in::sin_addr)&)(Address.Ipv4);
+            Addr4.sin_port = htons(Address.Port);
+            AddrLen = sizeof(sockaddr_in);
+        } else if (Address.IsV6()) {
+            auto & Addr6 = (sockaddr_in6&)AddrStorage;
+            Addr6.sin6_family = AF_INET6;
+            Addr6.sin6_addr = (decltype(sockaddr_in6::sin6_addr)&)(Address.Ipv6);
+            Addr6.sin6_port = htons(Address.Port);
+            AddrLen = sizeof(sockaddr_in6);
+        }
 
-
-
+		sendto(_Socket, DataPtr, DataSize, 0, (const sockaddr*)&AddrStorage, (socklen_t)AddrLen);
+	}
 
 }
