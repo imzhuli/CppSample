9b0d9aac1a8c2403b908f3026f6292c254a48908
zhuli <imzhuli@vip.qq.com>
2023-07-27 16:15:01 +0800

    minor type change

--- lib/include/xel/Byte.hpp
+++ lib/include/xel/Byte.hpp
@@ -163,11 +163,11 @@ X_NS
 		X_INLINE void WDL(double d)                        { __detail__::__raw__::UD ud{.d = d}; W8L(ud.u); }
 
 		X_INLINE void *          operator ()() const          { return _curr; }
-		X_INLINE operator        ubyte * () const             { return _curr; }
-		X_INLINE ubyte *         Origin() const               { return _start; }
+		X_INLINE operator        void * () const              { return _curr; }
+		X_INLINE void *          Origin() const               { return _start; }
 		X_INLINE ptrdiff_t       Offset() const               { return _curr - _start; }
 		X_INLINE void            Offset(ptrdiff_t offset)     { _curr = _start + offset; }
-		X_INLINE ubyte *         Skip(ptrdiff_t len)          { ubyte * from = _curr; _curr += len; return from; }
+		X_INLINE void *          Skip(ptrdiff_t len)          { ubyte * from = _curr; _curr += len; return from; }
 
 		X_INLINE void Reset()                              { _curr = _start; }
 		X_INLINE void Reset(void * s)                      { _curr = _start = static_cast<ubyte *>(s); }
@@ -202,11 +202,11 @@ X_NS
 		X_INLINE double   RDL()                            { __detail__::__raw__::UD ud{.u=R8L()}; return ud.d; }
 
 		X_INLINE const void *      operator ()() const        { return _curr; }
-		X_INLINE operator          const ubyte * () const     { return _curr; }
-		X_INLINE const ubyte *     Origin() const             { return _start; }
+		X_INLINE operator          const void * () const      { return _curr; }
+		X_INLINE const void *     Origin() const              { return _start; }
 		X_INLINE ptrdiff_t         Offset() const             { return _curr - _start; }
 		X_INLINE void              Offset(ptrdiff_t offset)   { _curr = _start + offset; }
-		X_INLINE const ubyte *     Skip(ptrdiff_t len)        { const ubyte * from = _curr; _curr += len; return from; }
+		X_INLINE const void *     Skip(ptrdiff_t len)         { const ubyte * from = _curr; _curr += len; return from; }
 
 		X_INLINE void Reset()                              { _curr = _start; }
 		X_INLINE void Reset(const void * s)                { _curr = _start = static_cast<const ubyte *>(s); }
